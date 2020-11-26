c2bb015c6015020ed7ba1757373c740e5b29a3e7
ShkAnna <contact@annashk.com>
2020-11-26 15:58:05 +0100

    Add q1.pl

---
+++ q1.pl
@@ -0,0 +1,24 @@
+:- op(20,xfy,?=).
+
+% Prédicats d'affichage fournis
+
+% set_echo: ce prédicat active l'affichage par le prédicat echo
+set_echo :- assert(echo_on).
+
+% clr_echo: ce prédicat inhibe l'affichage par le prédicat echo
+clr_echo :- retractall(echo_on).
+
+% echo(T): si le flag echo_on est positionné, echo(T) affiche le terme T
+%          sinon, echo(T) réussit simplement en ne faisant rien.
+
+echo(T) :- echo_on, !, write(T).
+echo(_).
+
+regle(E, rename) :-
+    split(E, G, D),
+    var(G),
+    var(D).
+
+split(E, G, D) :-
+    arg(1, E, G),
+    arg(2, E, D).
