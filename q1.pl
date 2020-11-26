30c8c2ffab1f2682e9b7cae9f2bb7840addc16d2
ShkAnna <contact@annashk.com>
2020-11-26 17:04:37 +0100

    Update q1

--- q1.pl
+++ q1.pl
@@ -14,11 +14,51 @@ clr_echo :- retractall(echo_on).
 echo(T) :- echo_on, !, write(T).
 echo(_).
 
-regle(E, rename) :-
-    split(E, G, D),
-    var(G),
-    var(D).
-
 split(E, G, D) :-
     arg(1, E, G),
     arg(2, E, D).
+
+regle(E, rename) :-
+    split(E, G, D),
+    var(D),
+    var(G).
+
+regle(E, simplify) :-
+    split(E, G, D),
+    var(G),
+    not(var(D)),
+    not(compound(D)).
+
+regle(E, expand) :-
+    split(E, G, D),
+    var(G),
+    compound(D),
+    not(occur_check(G, D)).
+
+%occur_check(G, D) :-.
+
+regle(E, check) :-
+    split(E, G, D),
+    G \== D,
+    occur_check(G, D).
+
+regle(E, orient) :-
+    split(E, G, D),
+    not(var(G)),
+    var(D).
+
+regle(E, decompose) :-
+    split(E, G, D),
+    func(G,D).
+
+regle(E, clash) :-
+    split(E, G, D),
+    not(func(G, D)).
+
+func(G, D) :-
+    compound(G),
+    compound(D),
+    functor(G, NameG, ArityG),
+    functor(D, NameD, ArityD),
+    NameG == NameD,
+    ArityG == ArityD.
